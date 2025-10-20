278b87bc88bcec30927e3a39e82de3fe5e4caef8
Vic-k-c <viqheruiyot@gmail.com>
2025-10-20 19:38:23 +0300

    Update distributors.js
    uncomment later: //const distributorService = require('../services/distributorService');

--- mikrotik-api-middleware/routes/distributors.js
+++ mikrotik-api-middleware/routes/distributors.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const distributorService = require('../services/distributorService');
+//const distributorService = require('../services/distributorService');
 
 // ===========================
 // POST - Create a new distributor
