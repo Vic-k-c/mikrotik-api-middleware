f163908e6f622bb517d6fceefc31e479f38babd0
Vic-k-c <viqheruiyot@gmail.com>
2025-10-21 02:32:55 +0300

    Update index.js

--- mikrotik-api-middleware/index.js
+++ mikrotik-api-middleware/index.js
@@ -24,6 +24,5 @@ app.get('/', (req, res) => {
 
 // Start server
 app.listen(port, () => {
-  console.log(`🚀 Server running at http://localhost:${port}`);
+  console.log(`🚀 Server running on port ${port}`);
 });
-
